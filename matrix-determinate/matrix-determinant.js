2320acc2b75562c573e334db9d6db946fef7206e
Shane Hobson <shanehobson1@gmail.com>
2018-06-21 23:44:45 -0400

    Added new algorithm

---
+++ matrix-determinate/matrix-determinant.js
@@ -0,0 +1,25 @@
+function determinant(m) {
+    if (m.length === 1) {
+      return m[0][0];           
+    } else {
+        let calcInput = [];
+        m[0].forEach((topRowElement, i) => {
+          let minor = [];
+          m.forEach((row, j) => {
+            if (j !== 0) {
+              minor.push(row.filter((rowElement, k) => {
+                return i !== k;
+              }));
+            }
+          });
+          calcInput.push(m[0][i] * determinant(minor));
+        });
+        return calcInput.reduce((acc, cur, currentIndex) => {
+          if (currentIndex % 2) {
+            return acc - cur;
+          } else {
+            return acc + cur;
+          }
+        });
+    }
+  };
