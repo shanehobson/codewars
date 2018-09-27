4f298a3e1dd056b613ad3cec2dc78b7ed495e541
Shane Hobson <shanehobson1@gmail.com>
2018-09-27 11:34:42 -0700

    Refactored algorithm

--- extract_domain_name_from_url/index.js
+++ extract_domain_name_from_url/index.js
@@ -1,19 +1,7 @@
-function domainName(url){
-    let startIndex = 0;
-    let endIndex = url.length - 1;
-    if (url.indexOf('https://') === 0) {
-      startIndex = (url.indexOf('//www.') > -1) ? 12 : 8;
-    } else if (url.indexOf('http://') === 0) {
-      startIndex = (url.indexOf('//www.') > -1) ? 11 : 7;
-    } else if (url.indexOf('www.') === 0) {
-      startIndex = 4;
-    } 
-    for (let i = startIndex; i < url.length; i++) {
-      if (url.charAt(i) === '.' || url.charAt(i) === '/') {
-        endIndex = i;
-        break;
-      }
-    }
-    return url.substring(startIndex, endIndex);
-  }
-  
+function extractDomainName(url) {
+  const regex = /(https?:\/\/)|(www.)/gi;
+  const shortUrl = url.replace(regex, '');
+  const endIndex = shortUrl.indexOf('.');
+  return shortUrl.slice(0, endIndex);
+}
+
